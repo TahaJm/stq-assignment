efc504ee2f42ec8ff7e4732b382c371ded641f2b
taha.jamshidi <tahajamshidi58@gmail.com>
2024-08-05 01:44:28 +0330

    feat(components): add city search component

---
+++ src/components/CitiesSearch/CitiesSearch.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Autocomplete from '@/components/AutoComplete/AutoComplete.vue'
+import { useCityStore } from '@/stores/cityStore'
+
+const cityStore = useCityStore()
+const getCityResultKey = (result: string) => result
+</script>
+
+<template>
+  <Autocomplete
+    label="Search Cities"
+    placeholder="Search cities..."
+    :fetchResults="cityStore.filterCities"
+    :results="cityStore.cityResults"
+    :getResultKey="getCityResultKey"
+  >
+    <template #default="{ result }">
+      {{ result }}
+    </template>
+    <template #selected="{ result }">
+      <div>Selected City: {{ result }}</div>
+    </template>
+  </Autocomplete>
+</template>
