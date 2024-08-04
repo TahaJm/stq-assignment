1b40c7c289ccfb85d90d9cd2bac184aedf81f755
taha.jamshidi <tahajamshidi58@gmail.com>
2024-08-05 01:45:43 +0330

    feat: add home page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import BookSearch from '@/components/BookSearch/BookSearch.vue'
+import CitiesSearch from '@/components/CitiesSearch/CitiesSearch.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    CitiesSearch,
+    BookSearch
+  }
+})
+</script>
 
 <template>
-  <header></header>
-  <main></main>
+  <header>
+    <h1>City and Book Search</h1>
+  </header>
+  <main>
+    <BookSearch />
+    <CitiesSearch />
+  </main>
 </template>
 
-<style scoped></style>
