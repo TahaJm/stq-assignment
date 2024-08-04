609c65c527b5f2f79610b6aeaabdacce21995844
taha.jamshidi <tahajamshidi58@gmail.com>
2024-08-05 01:44:17 +0330

    feat(components): add book search component

---
+++ src/components/BookSearch/BookSearch.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useBookStore } from '@/stores/bookStore'
+import Autocomplete from '@/components/AutoComplete/AutoComplete.vue'
+
+const bookStore = useBookStore()
+
+const getBookResultKey = (result: any) => result.title
+</script>
+
+<template>
+  <Autocomplete
+    label="Search Books"
+    placeholder="Search books..."
+    :fetchResults="bookStore.filterBooks"
+    :results="bookStore.bookResults"
+    :getResultKey="getBookResultKey"
+  >
+    <template #default="{ result }">
+      <strong>{{ result.title }}</strong> by {{ result.author }}
+    </template>
+    <template #selected="{ result }">
+      <div>
+        Selected Book: <strong>{{ result.title }}</strong> by {{ result.author }}
+      </div>
+    </template>
+  </Autocomplete>
+</template>
